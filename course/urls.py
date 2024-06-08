92e0e0a0d8ccadb2ad7eb00d96657ce547ca1a23
waisumko <waisumko@gmail.com>
2024-06-08 14:35:46 +0800

    update title

--- course/urls.py
+++ course/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.courseListPage, name="courseListPage"),
+    path('', views.courseListPage, name="ebookListPage"),
     path('download/<int:pk>', views.downloadFile, name='downloadPage'),
 ]
--- course/views.py
+++ course/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import CourseModel
 from django.http import FileResponse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
 def courseListPage(request):
     data = CourseModel.objects.all().order_by("-pk")
     return render(request, 'coursePage.html',{"data":data})
